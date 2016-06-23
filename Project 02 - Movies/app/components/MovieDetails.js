e2e64a59bfa82f0d16d3d83ff2277646d90468f3
Shawn Pan <xpan1@swarthmore.edu>
2016-06-22 18:21:46 -0700

    encapsulated omg url in a new function

--- Project 02 - Movies/app/components/MovieDetails.js
+++ Project 02 - Movies/app/components/MovieDetails.js
@@ -2,11 +2,20 @@ import React, { Component, PropTypes } from 'react'
 import { View, Text, Image, StyleSheet } from 'react-native'
 
 class MovieDetails extends Component {
+	constructor(props) {
+		super(props)
+		this.renderImageURL = this.renderImageURL.bind(this)
+	}
+
+	renderImageURL(url) {
+		return 'http://image.tmdb.org/t/p/w185/' + url		
+	} 
+
 	render() {
 		return (
 			<View style={styles.details}>
 				<Image 
-					source={{uri: 'http://image.tmdb.org/t/p/w185/' + this.props.imageUrl}} 
+					source={{ uri: this.renderImageURL(this.props.imageUrl)}} 
 					style={{width: 185, height: 250}}
 				/>
 				<Text>{this.props.overview}</Text>
