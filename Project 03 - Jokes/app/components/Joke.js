020c0fa474052490124ab0821ec37bc086497397
Shawn Pan <xpan1@swarthmore.edu>
2016-06-28 15:51:21 -0700

    PanResponder worked. Scale was incorrect

--- Project 03 - Jokes/app/components/Joke.js
+++ Project 03 - Jokes/app/components/Joke.js
@@ -13,6 +13,54 @@ var Joke = React.createClass({
     FirstName: PropTypes.string.isRequired,
     LastName: PropTypes.string.isRequired,
   },
+  getInitialState: function() {
+    return {
+      x: 0,
+      y: 0
+    }
+  },
+
+  componentWillMount: function() {
+    this._panResponder = PanResponder.create({
+      onStartShouldSetPanResponder: this._onStartShouldSetResponder,
+      onStartShouldSetPanResponderCapture: (evt, gestureState) => true,
+      onMoveShouldSetPanResponder: (evt, gestureState) => true,
+      onMoveShouldSetPanResponderCapture: (evt, gestureState) => true,
+      onPanResponderMove: this.setPosition,
+      onPanResponderRelease: this.resetPosition,
+    })
+  },
+
+  setPosition: function(nativeEvent, gestureState) {
+    //Update our state with the deltaX/deltaY of the movement
+    this.setState({
+      x: this.state.x + gestureState.dx,
+      y: this.state.y + gestureState.dy
+    });
+    //Set our drag to be the new position so our delta can be calculated next time correctly
+    console.log('this.state.x is' + this.state.x)
+  },
+  resetPosition: function(nativeEvent, gestureState) {
+    this.dragging = false;
+    //Reset on release
+    this.setState({
+      x: 0,
+      y: 0,
+    })
+  },
+  _onStartShouldSetResponder: function(nativeEvent, gestureState) {
+    this.dragging = true;
+    //Setup initial drag coordinates
+    console.log('in onStart, this.state.x is' + this.state.x)
+    return true;
+  },
+  _onMoveShouldSetResponder: function(nativeEvent, gestureState) {
+    return true;
+  },
+  getJokeStyle: function() {
+    var transform = [{translateX: this.state.x}, {translateY: this.state.y}];
+    return {transform: transform};
+  },
 
   parseJoke(num) {
     var str = jokes[num].joke
@@ -27,10 +75,11 @@ var Joke = React.createClass({
 
   render() {
     return (
-      <View style={styles.container}>
-        <Text ref={(joke) => {
-            this.joke = joke;
-          }} style={styles.text}>{this.renderJoke()}</Text>
+      <View 
+        {...this._panResponder.panHandlers}
+        style={[styles.container, this.getJokeStyle()]}>
+        <Text
+          style={ styles.text }>JOKE HEREE HEREAREWAREAWREARSADF</Text>
       </View>
     );
   }
