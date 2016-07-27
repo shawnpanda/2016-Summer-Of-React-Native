c6cfd88dc504c3ae7451426a2e32eeb7675bda0e
Shawn Pan <xpan1@swarthmore.edu>
2016-07-27 16:11:48 +0800

    add button in home

--- Project 06 - Offline Storage/app/App.js
+++ Project 06 - Offline Storage/app/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import {Scene, Router} from 'react-native-router-flux'
+import {Scene, Router, Actions} from 'react-native-router-flux'
 import { Provider, connect } from 'react-redux'
 import configureStore from './store/configureStore'
 import HomeContainer from './containers/HomeContainer'
@@ -10,10 +10,12 @@ const RouterWithRedux = connect()(Router)
 
 class OfflineStorage extends Component {
   render() {
+    const addNote = () => {Actions.edit()}
     return <Provider store={store}> 
     <RouterWithRedux>
-      <Scene key='home' component={HomeContainer} title='Home' initial={true}/>
-      <Scene key='edit' component={Edit}/>
+      <Scene key='home' component={HomeContainer} 
+          title='Home' initial={true} onRight={addNote} rightTitle="Add"/>
+      <Scene key='edit' title='Add' component={Edit}/>
     </RouterWithRedux>
     </Provider>
   }
